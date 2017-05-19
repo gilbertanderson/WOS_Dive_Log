5e7e51bde4e1a236d8861650b6611c5d0c48d74f
Dominique Riley <dtr9323@gmail.com>
2017-05-19 10:49:25 -0500

    added firebase functions for authorized users in the diveApp

--- diveLog/app.js
+++ diveLog/app.js
@@ -1,6 +1,136 @@
-firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});
+
+
+'use strict';
+<script type="text/javascript">
+  /**
+   * Handles the sign in button press.
+   */
+  function toggleSignIn() {
+    if (firebase.auth().currentUser) {
+      firebase.auth().signOut();
+      // [END signout]
+    } else {
+      var email = document.getElementById('email').value;
+      var password = document.getElementById('password').value;
+      if (email.length < 4) {
+        alert('Please enter an email address.');
+        return;
+      }
+      if (password.length < 4) {
+        alert('Please enter a password.');
+        return;
+      }
+      // Sign in with email and pass.
+      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode === 'auth/wrong-password') {
+          alert('Wrong password.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+        document.getElementById('quickstart-sign-in').disabled = false;
+        // [END_EXCLUDE]
+      });
+      // [END authwithemail]
+    }
+    document.getElementById('quickstart-sign-in').disabled = true;
+  }
+  /**
+   * Handles the sign up button press.
+   */
+  function handleSignUp() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    if (email.length < 4) {
+      alert('Please enter an email address.');
+      return;
+    }
+    if (password.length < 4) {
+      alert('Please enter a password.');
+      return;
+    }
+    // Sign in with email and pass.
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode == 'auth/weak-password') {
+        alert('The password is too weak.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+    });
+  }
+  /**
+   * Sends an email verification to the user.
+   */
+  function sendEmailVerification() {
+    // [START sendemailverification]
+    firebase.auth().currentUser.sendEmailVerification().then(function() {
+      // Email Verification sent!
+      alert('Email Verification Sent!');
+      // [END_EXCLUDE]
+    });
+  }
+  function sendPasswordReset() {
+    var email = document.getElementById('email').value;
+    // [START sendpasswordemail]
+    firebase.auth().sendPasswordResetEmail(email).then(function() {
+      // Password Reset Email Sent!
+      // [START_EXCLUDE]
+      alert('Password Reset Email Sent!');
+      // [END_EXCLUDE]
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // [START_EXCLUDE]
+      if (errorCode == 'auth/invalid-email') {
+        alert(errorMessage);
+      } else if (errorCode == 'auth/user-not-found') {
+        alert(errorMessage);
+      }
+      console.log(error);
+      // [END_EXCLUDE]
+    });
+    // [END sendpasswordemail];
+  }
+
+  function initApp() {
+    // Listening for auth state changes.
+    firebase.auth().onAuthStateChanged(function(user) {
+      document.getElementById('quickstart-verify-email').disabled = true;
+      if (user) {
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var providerData = user.providerData;
+        document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+        document.getElementById('quickstart-sign-in').textContent = 'Sign out';
+        document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+        if (!emailVerified) {
+          document.getElementById('quickstart-verify-email').disabled = false;
+        }
+      } else {
+        // User is signed out.
+        document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
+        document.getElementById('quickstart-sign-in').textContent = 'Sign in';
+        document.getElementById('quickstart-account-details').textContent = 'null';
+      }
+      document.getElementById('quickstart-sign-in').disabled = false;
+    });
+    document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, true);
+    document.getElementById('quickstart-sign-up').addEventListener('click', handleSignUp, true);
+    document.getElementById('quickstart-verify-email').addEventListener('click', sendEmailVerification, false);
+    document.getElementById('quickstart-password-reset').addEventListener('click', sendPasswordReset, false);
+  }
+  window.onload = function() {
+    initApp();
+  };
+</script>
