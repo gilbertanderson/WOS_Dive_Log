9e8e956b530856b76ff90c0344c31dbd8dfcc31e
Gilbert Anderson <gilbert.anderson@ge.com>
2017-05-19 20:11:26 -0500

    restructure

--- karma.conf.js
+++ karma.conf.js
@@ -2,13 +2,13 @@
 module.exports = function(config) {
   config.set({
 
-    basePath: './app',
+    basePath: 'app',
 
     files: [
       'bower_components/angular/angular.js',
-      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'components/**/*.js',
+      'app/components/**/*.js',
       'view*/**/*.js'
     ],
 
