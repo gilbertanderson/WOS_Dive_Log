56fa3be966be4201f30e22c4a65846f0cf53ddea
Gilbert Anderson <gilbert.anderson@ge.com>
2017-06-15 11:23:27 -0500

    UI edits to index.html

--- public/js/main.js
+++ public/js/main.js
@@ -28,12 +28,12 @@ angular.module('diveLog', ['ui.router'])
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    // document.getElementById("main").style.marginLeft = "250px";
     // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    // document.getElementById("main").style.marginLeft = "0";
     // document.body.style.backgroundColor = "white";
 }
