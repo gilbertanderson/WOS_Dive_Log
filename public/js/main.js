ad6c2c1b0584c471b6ef9960fd1f2ccc88d99cd3
Gilbert Anderson <gilbert.anderson@ge.com>
2017-06-15 16:33:26 -0500

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,39 @@
+/**
+ * Created by gilbertanderson on 6/12/17.
+ */
+
+angular.module('diveLog', ['ui.router'])
+
+    .config(['$stateProvider', '$urlRouterProvider',
+        function ($stateProvider, $urlRouterProvider) {
+            $urlRouterProvider.otherwise('/home');
+            let states = [
+                {
+                    name: 'home',
+                    url: '/home',
+                    views: {
+                        'nav@': {templateUrl: './templates/assets/nav.html'},
+                        'body@': {templateUrl: './templates/assets/body.html'},
+                        'footer@': {templateUrl: './templates/assets/footer.html'},
+                        'sidenav@': {templateUrl: './templates/assets/sidenav.html'}
+                    }
+                }
+            ];
+
+            states.forEach(function (state) {
+                $stateProvider.state(state);
+            });
+        }
+    ]);
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    // document.getElementById("main").style.marginLeft = "250px";
+    // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    // document.getElementById("main").style.marginLeft = "0";
+    // document.body.style.backgroundColor = "white";
+}
