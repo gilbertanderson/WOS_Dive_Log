4a116b9b2d9335bb779b8ddcb8ab14807b66c2a7
Gilbert Anderson <gilbert.anderson@ge.com>
2017-06-23 13:03:20 -0500

    edit app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,15 @@
 /**
  * Created by gilbertanderson on 6/12/17.
  */
-//app login goes here
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    // document.getElementById("main").style.marginLeft = "250px";
+    // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    // document.getElementById("main").style.marginLeft = "0";
+    // document.body.style.backgroundColor = "white";
+}
